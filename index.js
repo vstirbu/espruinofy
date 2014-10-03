d8ddfafcd7ee4785a9d43ddf4e4a92a6720c9999
Vlad Stirbu <vstirbu@gmail.com>
2014-10-03 11:34:33 +0300

    initial version

---
+++ index.js
@@ -0,0 +1,21 @@
+var Transform = require('stream').Transform;
+
+module.exports = function (b, opts) {
+  var estream = new Transform(),
+      start = true;
+
+  estream._transform = function (chunk, encoding, done) {
+    if (start) {
+      start = false;
+      estream.push('function onInit() {');
+    }
+    estream.push(chunk);
+    done(null);
+  };
+
+  estream._flush = function () {
+    estream.push('} onInit();');
+  }
+
+  b.pipeline.push(estream);
+}
